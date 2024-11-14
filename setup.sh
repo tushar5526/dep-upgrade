9102044a8598f38d1edad2c8ebc4959fe65dc328
Tushar Gupta <tushar.gupta@canoical.com>
2024-11-14 19:26:47 +0530

    init

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+mkdir ~/launchpad
+cd ~/launchpad
+curl https://git.launchpad.net/launchpad/plain/utilities/rocketfuel-setup > rocketfuel-setup
+chmod a+x rocketfuel-setup
+# specify a random non existing user to create a generic LXD image.
+# we will do user-specific modifications in cloud-init of the dev profile
+./rocketfuel-setup --assume-yes --lpusername lp-non-existing-user
+cd launchpad
+# Allow connections from outside hosts to be able to access LP from host system
+make LISTEN_ADDRESS='*' install
+./utilities/launchpad-database-setup ubuntu
+make schema
+
